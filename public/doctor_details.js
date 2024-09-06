3790a4904b61653d5f36f73f125d25ce6e0ef256
VenkataSaketh09 <22pa1a12b4@vishnu.edu.in>
2024-09-06 09:41:36 +0530

    first commit

---
+++ public/doctor_details.js
@@ -0,0 +1,25 @@
+// Array to store doctor details
+const doctors = [
+    {
+        email: "sailu@gmail.com",
+        name: "SAILAJA",
+        password: "hisailu",
+        phone: "9059917427",
+        specialist: "Neurologist",
+        patients: [
+            { name: "Badri", age: 20, condition: "Epilepsy", email: "badri@gmail.com" },
+            { name: "Dhanush", age: 23, condition: "Migraine", email: "maari@gmail.com" }
+        ]
+    },
+    {
+        email: "hari@gmail.com",
+        name: "HARISH",
+        password: "hisari",
+        phone: "8123456789",
+        specialist: "Cardiologist",
+        patients: [
+            { name: "Karthik", age: 24, condition: "Heart Disease", email: "karthik@gmail.com" },
+            { name: "Siva", age: 49, condition: "Hypertension", email: "siva@gmail.com" }
+        ]
+    }
+];
